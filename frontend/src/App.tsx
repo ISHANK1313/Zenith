cb9f99dfab419ed3140e6b66c4b7eb96d9b3a76b
ISHANK1313 <ishankp3@gmail.com>
2026-01-02 15:38:38 +0530

    adds missing react framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store/authStore'
 import { ROUTES } from './utils/constants'
